eaa0950f35b1aebcdcb4d276dbf9e87b5c7b92ae
GyuHyeong <cjsworbehd13@gmail.com>
2023-02-14 21:26:13 +0900

    메타Annotation 추가

--- src/test/java/com/example/helloboot/HelloServiceTest.java
+++ src/test/java/com/example/helloboot/HelloServiceTest.java
@@ -3,8 +3,26 @@ package com.example.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Test
+@interface UnitTest {
+
+}
 public class HelloServiceTest {
-    @Test
+    @FastUnitTest
     void simpleHelloService() {
         // given
         SimpleHelloService helloService = new SimpleHelloService();
