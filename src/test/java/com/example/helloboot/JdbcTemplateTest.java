197915cd86fd23e6e74711cc463010cb2d817cdb
GyuHyeong <cjsworbehd13@gmail.com>
2023-02-26 14:05:58 +0900

    1. 설정변경

--- src/test/java/com/example/helloboot/JdbcTemplateTest.java
+++ src/test/java/com/example/helloboot/JdbcTemplateTest.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class JdbcTemplateTest {
     @Autowired
     JdbcTemplate jdbcTemplate;
+    //;;
 
     @BeforeEach
     void init() {
