b65af60eb00606a50e3c460cbbad8f64b8dac968
GyuHyeong <cjsworbehd13@gmail.com>
2023-03-01 23:22:31 +0900

    1. 설정추가및 변경

--- src/test/java/com/example/helloboot/JdbcTemplateTest.java
+++ src/test/java/com/example/helloboot/JdbcTemplateTest.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class JdbcTemplateTest {
     @Autowired
     JdbcTemplate jdbcTemplate;
-
+//;;;
     @BeforeEach
     void init() {
         jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
